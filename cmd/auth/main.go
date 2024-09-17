38bf31adbba91ccd5c422c7f5d193b8169fa49df
gorik0 <delavarus@gmail.com>
2024-09-17 13:03:23 +0300

    db_connect

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"log"
 	"log/slog"
 	"os"
 	"ozon_replic/internal/pkg/config"
 	"ozon_replic/internal/pkg/utils/logger"
+	"ozon_replic/internal/pkg/utils/logger/sl"
 )
 
 func main() {
@@ -42,7 +45,33 @@ func run() error {
 		slog.String("log_file_path", cfg.LogFilePath),
 	)
 	log.Debug("debug messages are enabled")
+	//::::::::
+
+	//::::::::
 	//	::: inti  DB
+	db, err := pgxpool.Connect(context.Background(), fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable",
+		cfg.DBUser,
+		cfg.DBPass,
+		cfg.DBHost,
+		cfg.DBPort,
+		cfg.DBName))
+	if err != nil {
+		log.Error("fail open postgres", sl.Err(err))
+		err = fmt.Errorf("error happened in sql.Open: %w", err)
+
+		return err
+	}
+	defer db.Close()
+
+	if err = db.Ping(context.Background()); err != nil {
+		log.Error("fail ping postgres", sl.Err(err))
+		err = fmt.Errorf("error happened in db.Ping: %w", err)
+
+		return err
+	}
+	//::::::::
+
+	//::::::::
 	//	::: inti  REPO
 	//	::: inti  USECASE
 	//	::: inti  HANDLER
