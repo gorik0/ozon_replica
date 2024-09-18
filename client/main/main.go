2dc51cd0613ca571998ea4744e8de1f7224e7c5a
gorik0 <delavarus@gmail.com>
2024-09-18 21:58:58 +0300

    log_cfg_done

---
+++ client/main/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+	"os"
+	"ozon_replic/internal/pkg/config"
+	"ozon_replic/internal/pkg/utils/logger"
+	"ozon_replic/internal/pkg/utils/logger/sl"
+)
+
+func main() {
+	if err := run(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func run() (err error) {
+	cfg := config.MustLoad() // TODO : dev-config.yaml -> readme.
+
+	logFile, err := os.OpenFile(cfg.LogFilePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println("fail open logFile", sl.Err(err))
+		err = fmt.Errorf("fail open logFile: %w", err)
+
+		return err
+	}
+	defer func() {
+		err = errors.Join(err, logFile.Close())
+	}()
+
+	log := logger.Set(cfg.Enviroment, logFile)
+
+	log.Info(
+		"starting zuzu-main",
+		slog.String("env", cfg.Enviroment),
+		slog.String("addr", cfg.Address),
+		slog.String("log_file_path", cfg.LogFilePath),
+		slog.String("photos_file_path", cfg.PhotosFilePath),
+	)
+	log.Debug("debug messages are enabled")
+	return err
+}
